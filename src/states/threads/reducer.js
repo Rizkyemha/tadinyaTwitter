bfb79e694e207ba59babe0d85e5b129092a35c56
Rizkyemha <rizkymh31@gmail.com>
2023-08-10 20:36:52 +0700

    add repairing script for reducer RECEIVE_THREADS

--- src/states/threads/reducer.js
+++ src/states/threads/reducer.js
@@ -3,7 +3,7 @@ import { ActionType } from "./action";
 function threadsReducer( threads= [], action={} ) {
     switch (action.type) {
         case ActionType.RECEIVE_THREADS:
-            return []
+            return action.payload.threads
         case ActionType.ADD_THREAD:
             return [action.payload.thread, ...threads]
         default:
